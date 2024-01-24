cae19dd93d493ac7eda9812a426185770e87ded3
ZiqiWei <1204021663@ybu.edu.cn>
2024-01-24 21:44:46 +0800

    first commoy

---
+++ .vitepress/config.mjs
@@ -0,0 +1,68 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/public/logo.svg" }]],
+  title: "DoliL in VitePress",
+  // 用来做SEO
+  description: "A VitePress Site",
+  themeConfig: {
+    outlineTitle: "文章目录",
+    outline: [2,6],
+    logo: '/public/logo.svg',
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'More', link: '/markdown-examples' },
+      { text: '侧边栏1', link: '/front-end/react' },
+      { text: '侧边栏2', link: '/backend/rabbitmq' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    // footer
+    footer: 
+      { copyright: "Copyright@ 2024 Dolimence Wei" },
+         // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+    
+  },
+  
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "DoliL in ViteSite"
+  text: "An Awesome Peoject"
+  tagline: 𝒞𝒶𝓊𝓈𝑒 𝐼'𝓁𝓁 𝒷𝓇𝑒𝒶𝓉𝒽𝑒. 𝒲𝒽𝑒𝓃 𝓉𝒽𝑒𝓎 𝓉𝓇𝓎 𝓉𝑜 𝓈𝓊𝒻𝒻𝑜𝒸𝒶𝓉𝑒 𝓂𝑒. 𝒟𝑜𝓃'𝓉 𝓎𝑜𝓊 𝓊𝓃𝒹𝑒𝓇𝑒𝓈𝓉𝒾𝓂𝒶𝓉𝑒 𝓂𝑒 .𝒞𝒶𝓊𝓈𝑒 𝐼 𝓀𝓃𝑜𝓌 𝓉𝒽𝒶𝓉 𝐼 𝓌𝑜𝓃'𝓉 𝑔𝑜 𝓈𝓅𝑒𝑒𝒸𝒽𝓁𝑒𝓈𝓈.
+  image:
+    src: /index-image2.svg
+    alt: DoliL in ViteSite
+  actions:
+    - theme: brand
+      text: Goto Notion
+      link: https://www.notion.so/dolimence/DoliL-2bb88dd5ba57444c93305e96a458517f
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
