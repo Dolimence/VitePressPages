6db4f1041851540a5e12704760771339544222d6
ZiqiWei <1204021663@ybu.edu.cn>
2024-02-01 17:50:23 +0800

    title icon

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   // base: "/VitePressPages/",
-  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  head: [["link", { rel: "icon", href: "/VitePressPages/logo.svg" }]],
   title: "DoliL in VitePress",
   // 用来做SEO
   description: "A VitePress Site",
